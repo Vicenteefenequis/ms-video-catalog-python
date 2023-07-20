268c1327abbce1a79d14092b86e4dd79cf86f52e
Vicente Junior <vicente19981@live.com>
2023-07-20 20:02:16 +0000

    test: ensure Category invalid case for name prop

---
+++ src/category/tests/integration/domain/test_int_entities.py
@@ -0,0 +1,17 @@
+
+
+import unittest
+from __seedwork.domain.exceptions import ValidationException
+
+from category.domain.entities import Category
+
+
+class TestCategoryIntegration(unittest.TestCase):
+
+    def test_invalid_cases_for_name_prop(self):
+        with self.assertRaises(ValidationException) as assert_error:
+            Category(name=None)  # type: ignore
+        self.assertEqual(
+            assert_error.exception.args[0],
+            "The name is required"
+        )
