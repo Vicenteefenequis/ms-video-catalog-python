92301f0fe9be5db17871a6f9ed3a95e1788d5b71
Vicente Junior <vicente19981@live.com>
2023-07-20 20:06:25 +0000

    test: ensure Category invalid case for is_active prop

--- src/category/tests/integration/domain/test_int_entities.py
+++ src/category/tests/integration/domain/test_int_entities.py
@@ -36,3 +36,19 @@ class TestCategoryIntegration(unittest.TestCase):
             assert_error.exception.args[0],
             "The name must be less than 255 characters"
         )
+
+    def test_invalid_cases_for_description_prop(self):
+        with self.assertRaises(ValidationException) as assert_error:
+            Category(name='Movie', description=5)  # type: ignore
+        self.assertEqual(
+            assert_error.exception.args[0],
+            "The description must be a string"
+        )
+
+    def test_invalid_cases_for_is_active_prop(self):
+        with self.assertRaises(ValidationException) as assert_error:
+            Category(name='Movie', is_active=5)  # type: ignore
+        self.assertEqual(
+            assert_error.exception.args[0],
+            "The is_active must be a boolean"
+        )
