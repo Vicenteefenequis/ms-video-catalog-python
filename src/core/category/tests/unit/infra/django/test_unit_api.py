510b3c0beed42e35267385ff557bc3940d749254
vicente_nascimento <vicente19981@live.com>
2023-08-22 22:52:00 +0000

    test: ensure api list categories unit

--- src/core/category/tests/unit/infra/django/test_unit_api.py
+++ src/core/category/tests/unit/infra/django/test_unit_api.py
@@ -3,11 +3,13 @@
 from datetime import datetime
 import unittest
 from unittest import mock
+from core.category.application.dto import CategoryOutput
 from rest_framework.test import APIRequestFactory
 from rest_framework.request import Request
 
 from core.category.application.use_cases import (
-    CreateCategoryUseCase
+    CreateCategoryUseCase,
+    ListCategoriesUseCase
 )
 from core.category.infra.django.api import CategoryResource
 
@@ -26,10 +28,10 @@ class TestCategoryResourceUnit(unittest.TestCase):
             created_at=datetime.now()
         )
 
-        resource = CategoryResource(
-            list_use_case=None,  # type: ignore
-            create_use_case=lambda: mock_create_use_case
-        )
+        resource = CategoryResource(**{
+            **self.__init_all_none(),
+            'create_use_case': lambda: mock_create_use_case
+        })
         _request = APIRequestFactory().post('/', send_data)
         request = Request(_request)
         request._full_data = send_data  # pylint: disable=protected-access
@@ -46,3 +48,63 @@ class TestCategoryResourceUnit(unittest.TestCase):
             'is_active': True,
             'created_at': mock_create_use_case.execute.return_value.created_at
         })
+
+    def test_list_method(self):
+        mock_list_use_case = mock.Mock(ListCategoriesUseCase)
+
+        mock_list_use_case.execute.return_value = ListCategoriesUseCase.Output(
+            items=[
+                CategoryOutput(
+                    id='114e527b-d222-44f1-86c7-1cb621f44849',
+                    name='Movie',
+                    description=None,
+                    is_active=True,
+                    created_at=datetime.now()
+                )
+            ],
+            total=1,
+            current_page=1,
+            per_page=2,
+            last_page=1
+        )
+
+        resource = CategoryResource(**{
+            **self.__init_all_none(),
+            'list_use_case': lambda: mock_list_use_case
+        })
+        _request = APIRequestFactory().get(
+            '/?page=1&per_page=1&sort=name&sort_dir=asc&filter=test'
+        )
+        request = Request(_request)
+        response = resource.get(request)
+        mock_list_use_case.execute.assert_called_with(ListCategoriesUseCase.Input(
+            page="1",  # type: ignore
+            per_page="1",  # type: ignore
+            sort='name',
+            sort_dir='asc',
+            filter='test'
+        ))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.data, {
+            'items': [
+                {
+                    'id': '114e527b-d222-44f1-86c7-1cb621f44849',
+                    'name': 'Movie',
+                    'description': None,
+                    'is_active': True,
+                    'created_at': mock_list_use_case.execute.return_value.items[0].created_at
+                }
+            ],
+            'total': 1,
+            'current_page': 1,
+            'per_page': 2,
+            'last_page': 1
+        })
+
+    def __init_all_none(self):
+        return {
+            'list_use_case': None,
+            'create_use_case': None
+        }
