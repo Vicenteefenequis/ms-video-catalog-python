664e02d14f3f4e1aefc4c29ce30f5cf83ce95a14
vicente_nascimento <vicente19981@live.com>
2023-08-22 22:53:20 +0000

    refactor: removing no unused code

--- src/core/category/infra/django/urls.py
+++ src/core/category/infra/django/urls.py
@@ -1,27 +1,7 @@
 
 from django.urls import path
-from core.category.application.use_cases import CreateCategoryUseCase
-
-from core.category.infra.in_memory.repositories import CategoryInMemoryRepository
 from django_app import container
 from .api import CategoryResource
-
-
-class CategoryInMemoryRepositoryFactory:
-    repo: CategoryInMemoryRepository = None  # type: ignore
-
-    @classmethod
-    def create(cls):
-        if not cls.repo:
-            cls.repo = CategoryInMemoryRepository()
-        return cls.repo
-
-
-class CreateCategoryUseCaseFactory:
-    @staticmethod
-    def create():
-        repo = CategoryInMemoryRepositoryFactory.create()
-        return CreateCategoryUseCase(repo)
 
 
 urlpatterns = [
